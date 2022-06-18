8ad7a2b20f9ce1d7238736d45e81f73c89437941
Jake Finn <lof.steven@gmail.com>
2022-06-18 15:34:09 -0400

    Add text document that contains all the words needed, make test that checks if the file exists

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,29 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class MainTest {
+
+    @Test
+    void CheckForWords() throws FileNotFoundException {
+        File words = new File("src/main/hangman_words.txt");
+        assertTrue(words.exists());
+    }
+    @Test
+    void CheckIfWordIsRandmom() throws FileNotFoundException {
+        String word1 = Main.randomWord();
+        String word2 = Main.randomWord();
+        assertNotEquals(word1, word2);
+    }
+    @Test
+    void CheckGuess(){
+//        String word = Main.randomWord();
+//        List<Character> playerGuesses = new ArrayList<>();
+//        Main.getPlayerGuess("asdf", word, playerGuesses);
+    }
+}
