a9b4a14cb27a9c113e88af28a35f89e3cb041966
Jake Finn <lof.steven@gmail.com>
2022-06-19 15:02:24 -0400

    Print out hanged man based on wrong guesses

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -23,34 +25,93 @@ public class Main {
         }
 
         String word = randomWord();
-
-        System.out.println(word);
-
+        System.out.println("\nword = " + word);
         List<Character> playerGuesses = new ArrayList<>();
+        System.out.println("\nMissed Letters");
 
-        printWordState(word, playerGuesses);
-
+        int wrong = 0;
         while(true){
+            if (wrong == 0)
+            {
+                System.out.println(
+                        "\n+---+" +
+                        "\n    |" +
+                        "\n    |" +
+                        "\n    |" +
+                        "\n   ==="
+                );
+            }
+            else if (wrong == 1)
+            {
+                System.out.println(
+                        "\n+---+" +
+                        "\n0   |" +
+                        "\n    |" +
+                        "\n    |" +
+                        "\n   ==="
+                );
+            }
+            else if (wrong == 2)
+            {
+                System.out.println(
+                        "\n+---+" +
+                        "\n0   |" +
+                        "\n|   |" +
+                        "\n    |" +
+                        "\n   ==="
+                );
+            }
+            else if (wrong == 3)
+            {
+                System.out.println(
+                        "\n+---+" +
+                        "\n0   |" +
+                        "\n|   |" +
+                        "\n|   |" +
+                        "\n   ==="
+                );
+            }
+
+            printWordState(word, playerGuesses);
+
+                if (!getPlayerGuess(keyboard,word,playerGuesses)){
+                wrong++;
+                System.out.println("wrong count = " + wrong);
+            }
+
             getPlayerGuess(keyboard, word, playerGuesses);
+
+            if(printWordState(word, playerGuesses)){
+            System.out.println("\n\nYes! The scret word is \"" + word + "\"! You have won!" +
+                    "\nDo you want to play again? (yes or no)");
+                break;
+            }
         }
     }
 
-    public static void getPlayerGuess(Scanner keyboard, String word, List<Character> playerGuesses) {
+    public static boolean getPlayerGuess(Scanner keyboard, String word, List<Character> playerGuesses) {
         System.out.println("\nGuess a letter");
         String letterGuess = keyboard.nextLine();
-        playerGuesses.add(letterGuess.charAt(0));
+//        if (playerGuesses.contains(letterGuess)){
+//            System.out.println("You have already guessed that letter. Choose again.");
+//        } else {
+//            playerGuesses.add(letterGuess.charAt(0));
+//        }
 
-        printWordState(word, playerGuesses);
+        return word.contains(letterGuess);
     }
 
-    public static void printWordState(String word, List<Character> playerGuesses) {
+    public static boolean printWordState(String word, List<Character> playerGuesses) {
+        int correct = 0;
         for (int i = 0; i < word.length(); i++) {
             if (playerGuesses.contains((word.charAt(i)))) {
+                correct++;
                 System.out.print(word.charAt(i));
             } else {
                 System.out.print("_");
             }
         }
+        return (word.length()==correct);
     }
 
 }
